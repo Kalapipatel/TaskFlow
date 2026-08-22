f8685ac40b10bfad24b63cc6c4cd629486c2b149
Kalapi Patel <kalapivadariya03@gmail.com>
2026-08-22 12:02:32 +0530

    fisrt commit, basic structur is ready

---
+++ Backend/LabelService/src/test/java/com/TaskFlow/LabelService/LabelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskFlow.LabelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
